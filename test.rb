2f6ba319d6096cc707d49bcb4cf9143219f95abb
ekinohito <zhenyagur9@gmail.com>
2021-10-02 19:11:33 +0300

    refactoring

--- test.rb
+++ test.rb
@@ -3,6 +3,22 @@
 require 'minitest/autorun'
 require './main'
 
+INPUT = 'Lorem ipsum dolor sit amet,
+consectetur adipiscing elit. Curabitur tempor felis
+ut nisi sollicitudin, a pulvinar tortor rutrum.
+Nunc quis odio tellus. Fusce eget lobortis magna.
+Integer congue non velit hendrerit euismod. Sed nec
+dignissim sapien, vitae porttitor est. Nam tincidunt
+id nisi vitae cursus.'
+
+OUTPUT = '.susruc eativ isin di
+tnudicnit maN .tse rotittrop eativ ,neipas missingid
+cen deS .domsiue tirerdneh tilev non eugnoc regetnI
+.angam sitrobol tege ecsuF .sullet oido siuq cnuN
+.murtur rotrot ranivlup a ,niduticillos isin tu
+silef ropmet rutibaruC .tile gnicsipida rutetcesnoc
+,tema tis rolod muspi meroL'
+
 # Tests file operations
 class TestFiles < Minitest::Test
   def initialize(name)
@@ -28,21 +44,9 @@ class TestFiles < Minitest::Test
   end
 
   def test_file
-    File.write(@input_filename, 'Lorem ipsum dolor sit amet,
-consectetur adipiscing elit. Curabitur tempor felis
-ut nisi sollicitudin, a pulvinar tortor rutrum.
-Nunc quis odio tellus. Fusce eget lobortis magna.
-Integer congue non velit hendrerit euismod. Sed nec
-dignissim sapien, vitae porttitor est. Nam tincidunt
-id nisi vitae cursus.')
+    File.write(@input_filename, INPUT)
     reverse_file(@input_filename, @output_filename)
     assert_path_exists @output_filename
-    assert_equal '.susruc eativ isin di
-tnudicnit maN .tse rotittrop eativ ,neipas missingid
-cen deS .domsiue tirerdneh tilev non eugnoc regetnI
-.angam sitrobol tege ecsuF .sullet oido siuq cnuN
-.murtur rotrot ranivlup a ,niduticillos isin tu
-silef ropmet rutibaruC .tile gnicsipida rutetcesnoc
-,tema tis rolod muspi meroL', File.read(@output_filename)
+    assert_equal OUTPUT, File.read(@output_filename)
   end
 end
